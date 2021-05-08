a3ee50274bc733a1b159c54bf8dc443734a93020
Carlos Munoz <carlos.munoz@cornershopapp.com>
2021-05-08 02:00:45 -0700

    feat(imageapp): add base front end

---
+++ frontend/src/index.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const NavBarContainer = styled.section`
+  width: 100vh;
+  height: 50px;
+  position:absolute;
+  top:0px;
+  left:0px;
+  overflow:auto;
+  padding: auto auto;
+  margin: 0px 0 0px;
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+  justify-content: center;
+  background-color: black;
+`;
